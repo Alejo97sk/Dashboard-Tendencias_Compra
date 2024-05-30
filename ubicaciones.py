db6434eb84d5091ddca187a70dc55c40cb2ec29b
Alejo97sk <168129410+Alejo97sk@users.noreply.github.com>
2024-05-30 15:26:54 -0500

    ubicaciones.py

--- ubicaciones.py
+++ ubicaciones.py
@@ -1,14 +1,9 @@
 import pandas as pd
-import numpy as np
 import plotly.express as px
 import streamlit as st
-from streamlit.components.v1 import html
-
 
 def sales_header():
-
-    st.title('Ubicaciones :earth_americas:')
-
+    st.title('Ubicaciones :globe_with_meridians:')
 
 def filter_data(df, category_filter, size_filter, season_filter):
 
@@ -18,7 +13,6 @@ def filter_data(df, category_filter, size_filter, season_filter):
 
     return df[filt]
 
-
 # Function To Get The latitude and longitude for each Location
 @st.cache_data
 def load__locations_data():
@@ -27,45 +21,22 @@ def load__locations_data():
 
     return positions
 
-
 def create_map(the_df):
     pos = load__locations_data()
+    alt_df = the_df.merge(pos, left_on="Ubicación", right_on="name", how="left") \
+                  .groupby(["Ubicación", "latitude", "longitude"], as_index=False)["Precio_USD"].sum()
 
-    alt_df = the_df.merge(pos, left_on="Ubicación", right_on="name", how="left")
-    alt_df = alt_df.groupby(["Ubicación", "latitude", "longitude"], as_index=False)[
-        "Precio_USD"].sum()
-
-    fig = px.scatter_mapbox(alt_df, lat="latitude",
-                            lon="longitude", hover_name="Ubicación",
-                                hover_data=["Precio_USD"],
-                                color="Precio_USD",
-                            size="Precio_USD",
-                            color_continuous_scale=px.colors.cyclical.Twilight ,
-                            zoom=3,
-                            height=650,
-                            title="\t\tUbicaciones y sus precios de venta",
-                           
-                            )
-
+    fig = px.scatter_mapbox(alt_df, lat="latitude", lon="longitude", hover_name="Ubicación",
+                            hover_data=["Precio_USD"], color="Precio_USD", size="Precio_USD",
+                            color_continuous_scale=px.colors.cyclical.Twilight, zoom=3, height=650,
+                            title="Ubicaciones y sus precios de venta"
+    )
     fig.update_layout(
         mapbox_style="open-street-map",
-        title={
-            "font": {
-                "size": 25,
-                "family": "tahoma"
-            },
-            "x": 0.5,  # Centrar el título horizontalmente
-            "xanchor": "center"
-        },
-        hoverlabel={
-            "bgcolor": "#222",
-            "font_size": 15,
-            "font_family": "tahoma"
-        }
+        title={"text": "Ubicaciones y sus precios de venta", "font":{"size": 25, "family": "tahoma"}, "x": 0.5, "xanchor": "center"},
+        hoverlabel={"bgcolor": "#222", "font_size": 15, "font_family": "tahoma"}
     )
-
     return fig
-
 
 def get_top_n(the_df, n):
     top_n_location = the_df.groupby("Ubicación")["Precio_USD"]\
@@ -75,131 +46,56 @@ def get_top_n(the_df, n):
     filt = the_df["Ubicación"].isin(top_n_location)
     return the_df[filt]
 
-
 def create_subscription_via_location(the_df):
     purchases_by_loc = the_df.groupby("Ubicación")["Compras_Anteriores"].sum().nlargest(10)
-
-
-    fig = px.bar(purchases_by_loc,
-             orientation="h",
-         x = purchases_by_loc,
-         y = purchases_by_loc.index,
-         labels = {"x" : "Total Compras"},
-         text_auto="0.4s",  
-         title = "Top 10 Número de compras por Estado",
-         color = purchases_by_loc.index,
-         color_discrete_sequence=["#8DBBDF"],
-             template="plotly_dark"
-        )
-
+    fig = px.bar(purchases_by_loc, orientation="h", text_auto="0.4s", labels={"x": "Total Compras"},
+                 title="Top 10 Número de compras por Estado", color_discrete_sequence=["#8DBBDF"], template="plotly_dark"
+    )
     fig.update_layout(
-        showlegend = False,
-     title = {
-        "font": {
-            "size": 25,
-            "family": "tahoma",
-        },
-        "x": 0.5,  # Centrar el título horizontalmente
-        "xanchor": "center"
-    },
-     xaxis= dict(title='Total Compras',
-                 title_font=dict(size=18)),
-     yaxis=dict(title='Ubicación',
-                title_font=dict(size=18)),
-)
-
+        showlegend=False,
+        title={"text": "Top 10 Número de compras por Estado","font":{ "size": 25, "family": "tahoma"}, "x": 0.5, "xanchor": "center"},
+        xaxis=dict(title='Total Compras', title_font=dict(size=18)),
+        yaxis=dict(title='Ubicación', title_font=dict(size=18))
+    )
     fig.update_traces(
-        textfont= {
-        "family": "tahoma",
-        "size": 14,  
-        "color" : "#fff"
-    },
-    hovertemplate = "State:%{y}<br>Total Purchases: %{x:0.4s}",
-)
-
+        textfont={"family": "tahoma", "size": 14, "color": "#fff"},
+        hovertemplate="State: %{y}<br>Total Purchases: %{x:0.4s}"
+    )
     return fig
-
 
 def create_location_category(the_df):
     dff = get_top_n(the_df, 5)
     category_via_loc = dff.groupby("Ubicación", as_index=False)["Categoría"]\
-        .value_counts()
+    .value_counts()
 
-    fig = px.sunburst(category_via_loc, path=['Ubicación', 'Categoría'],
-                      values='count',
-                      color_discrete_sequence=[
-                          "#B403C2", "#7703C2", "#0329C2", "#03AEC2", "#03C251"],
-                      title="\t\tTop 5 estados y su popularidad de categorías"
-
-                      )
-
-    
+    fig = px.sunburst(category_via_loc, path=['Ubicación', 'Categoría'], values='count',
+                      color_discrete_sequence=["#B403C2", "#7703C2", "#0329C2", "#03AEC2", "#03C251"],
+                      title="Top 5 estados y su popularidad de categorías"
+    )
     fig.update_layout(
-        title={
-            "font": {
-                "size": 20,
-                "family": "tahoma"
-            },
-            "x": 0.5,  # Centrar el título horizontalmente
-            "xanchor": "center"
-        },
-        hoverlabel={
-            "bgcolor": "#222",
-            "font_size": 14,
-            "font_family": "tahoma"
-        },
-       
+        title={"text": "Top 5 estados y su popularidad de categorías","font":{ "size": 20, "family": "tahoma"}, "x": 0.5, "xanchor": "center"},
+        hoverlabel={"bgcolor": "#222", "font_size": 14, "font_family": "tahoma"}  
     )
-
     fig.update_traces(
-        textfont={
-            "family": "tahoma",
-            "size": 13,
-        },
-        hovertemplate="%{label}<br>Popularidad (%): %{value:.0f}%",
+        textfont={"family": "tahoma", "size": 13}, 
+        hovertemplate="%{label}<br>Popularidad (%): %{value:.0f}%"
     )
-
     return fig
-
 
 def create_top3_review(the_df):
     loc_review = the_df.groupby("Ubicación")["Calificación_de_Reseña"].mean().nlargest(3)
-    fig = px.bar(loc_review,
-                 color_discrete_sequence=["#049FE8"],
-                 template="plotly_dark",
-                 text_auto="0.1f",
-                 labels={
-                     "value": "PROM Calficación", "Estado_de_Suscripción": "Estado_de_Suscripción"},
-                 title="\t\tLos 3 estados con mejor valoración",
-                 )
-
+    fig = px.bar(loc_review, color_discrete_sequence=["#049FE8"], template="plotly_dark", text_auto="0.1f",
+                 labels={"value": "PROM Calficación", "Estado_de_Suscripción": "Estado_de_Suscripción"},
+                 title="Los 3 estados con mejor valoración"
+    )
     fig.update_layout(
         showlegend=False,
-        title={
-            "font": {
-                "size": 20,
-                "family": "tahoma"
-            },
-            "x": 0.5,  # Centrar el título horizontalmente
-            "xanchor": "center"
-        },
-        hoverlabel={
-            "bgcolor": "#222",
-            "font_size": 15,
-            "font_family": "tahoma"
-        },
-        xaxis= dict(title='Ubicación',
-                    title_font=dict(size=18)),
-        yaxis=dict(title='PROM Calificación',
-                   title_font=dict(size=18)),
+        title={"text": "Los 3 estados con mejor valoración", "font":{"size": 20, "family": "tahoma"}, "x": 0.5, "xanchor": "center"},
+        xaxis=dict(title='Ubicación', title_font=dict(size=18)),
+        yaxis=dict(title='PROM Calificación', title_font=dict(size=18))
     )
-
     fig.update_traces(
-        textfont={
-            "family": "tahoma",
-            "size": 15,
-        },
-        hovertemplate="State: %{x}<br>AVG Review: %{y:.1f}",
+        textfont={"family": "tahoma", "size": 15}, 
+        hovertemplate="State: %{x}<br>AVG Review: %{y:.1f}"
     )
-
     return fig
