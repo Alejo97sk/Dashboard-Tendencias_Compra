652fa747053fd2d1202ea252ecf0e5b2bbdab057
Alejo97sk <168129410+Alejo97sk@users.noreply.github.com>
2024-05-30 14:58:17 -0500

    productos.py

--- productos.py
+++ productos.py
@@ -1,86 +1,94 @@
-import pandas as pd
-import plotly.figure_factory as ff
+# Importing Libraries
 import plotly.express as px
 import streamlit as st
 
-def home_header():
-    st.title('Panel de Tendencias de Compra :chart_with_upwards_trend:')
+def products_header():
+    st.title("Productos :shopping_bags:")
 
 def filter_data(df, category_filter, size_filter, location_filter):
-    if location_filter == "ALL":
-        filt = (df["Categoría"].isin(category_filter)) &\
-            (df["Tamaño"].isin(size_filter))
-    else:
-        filt = (df["Categoría"].isin(category_filter)) &\
-            (df["Tamaño"].isin(size_filter)) &\
-            (df["Ubicación"] == location_filter)
+    filt = (df["Categoría"].isin(category_filter)) & (df["Tamaño"].isin(size_filter))
+    if location_filter != "ALL":
+        filt &= (df["Ubicación"] == location_filter)
     return df[filt]
 
-def total_customers(the_df):
-    return f"{len(the_df):,.0f}"
+def number_of_products(the_df):
+    return the_df["Artículo_Comprado"].nunique()
 
-def avergae_rating(the_df):
-    return round(the_df["Calificación_de_Reseña"].mean(), 1)
+def number_of_category(the_df):
+    return the_df["Categoría"].nunique()
 
-def total_purchases(the_df):
-    return f"{the_df['Compras_Anteriores'].sum():,.0f}"
+def total_sales(the_df):
+    return f'{the_df["Precio_USD"].sum():,.0f}'
 
-def create_category_chart(the_df):
-    category_per_season = the_df.pivot_table(index = "Temporada", columns = the_df["Categoría"], values ="Categoría", aggfunc="count")
-    total = category_per_season[category_per_season.columns].sum(1)
-    category_per_season.loc["Otoño"]  = round(category_per_season.loc["Otoño"] / total.values[0] * 100)
-    category_per_season.loc["Primavera"] = round(category_per_season.loc["Primavera"] / total.values[1] * 100)
-    category_per_season.loc["Verano"] = round(category_per_season.loc["Verano"] / total.values[2] * 100)
-    category_per_season.loc["Invierno"] = round(category_per_season.loc["Invierno"] / total.values[3] * 100)
-    fig = px.bar(category_per_season, barmode="group", x = category_per_season.index, y = category_per_season.columns,
-                 color_discrete_sequence=["#2F62CE","#4C98EC","#0B02C3","#75D2FD"], text_auto="%0.0f",
-                 title="Popularidad en % de Categoría por Temporada", labels={"value" : "Popularidad (%)","index":"Temporada"},
-     )
+def create_products_chart(the_df):
+    top_5_products = the_df["Artículo_Comprado"].value_counts().nlargest(5)
+    fig = px.bar(top_5_products,
+                 x=top_5_products.values,
+                 y=top_5_products.index,
+                 orientation="h",
+                 color=top_5_products.index,
+                 color_discrete_sequence=["#81C0FF","#B581FF","#81FFDB","#81FF85","#D478EB"],
+                 labels={"x": "Frecuencia de Artículos Vendidos", "y": "Artículo"},
+                 text_auto=True,
+                 title="Top 5 Artículos Vendidos"
+    )
+    fig.update_layout(
+        showlegend=False,
+        title={"font": {"size": 20, "family": "tahoma"}, "x": 0.5, "xanchor": "center"},
+        hoverlabel={"bgcolor": "#222", "font_size": 15, "font_family": "tahoma"},
+        yaxis={"title": 'Artículo Comprado', "tickfont": {"size": 15}, "title_font": {"size": 18}},
+        xaxis={"title": 'Frecuencia', "tickfont": {"size": 15}, "title_font": {"size": 18}}
+    )
+    fig.update_traces(
+       textfont={"family": "consolas", "size": 17, "color": "#fff"},
+       hovertemplate="Producto: %{x}<br>Popularidad (%): %{y:.1f}%"
+    )
+    return fig
+
+def create_size_chart(the_df):
+    the_df = the_df.dropna(subset=["Tamaño", "Género"])  # Asegurarse de que no haya valores nulos en "Tamaño" y "Género"
+    size_by_gender = the_df.pivot_table(index="Género", columns="Tamaño", values="Artículo_Comprado", aggfunc="count", fill_value=0)
+    size_by_gender = size_by_gender.stack().reset_index(name="Count")
+    custom_palette = ['#1AAFE3', '#8E21FB']
+    fig = px.sunburst(size_by_gender,
+                      path=['Género', 'Tamaño'],
+                      values='Count',
+                      color_discrete_sequence=custom_palette,
+                      title="Frecuencia de Tamaño por Género",
+    )
+
     fig.update_layout(
         title={"font": {"size": 20, "family": "tahoma"}, "x": 0.5, "xanchor": 'center'},
-        xaxis={"title": 'Temporada', "tickfont": {"size": 15, "family": 'tahoma'}, "title_font": {"size": 18}},
-        yaxis={"title": 'Popularidad (%)', "tickfont": {"size": 15, "family": 'tahoma'}, "title_font": {"size": 18}},
+        hoverlabel={"bgcolor": "black", "font_size": 15, "font_family": "tahoma"}
     )
+
     fig.update_traces(
-        textfont={"family": "tahoma", "size": 11, "color": "#fff"},
-        hovertemplate="%{label}<br>Popularidad (%): %{y}%"
+        textinfo='label+percent entry',
+        textfont={"family": "tahoma", "size": 15, "color": 'white'},
+        hovertemplate="Género: %{label}<br>Frecuencia: %{value:.0f}"
     )
     return fig
 
-def create_gender_chart(the_df):
-    discount_by_status = pd.crosstab(index = the_df["Estado_de_Suscripción"], 
-                                columns=the_df["Descuento_Aplicado"], 
-                                values=the_df["Descuento_Aplicado"], 
-                                 aggfunc="count", normalize=0) * 100
+def category_via_season_chart(the_df):
+    sales_per_category = the_df.groupby("Categoría")["Precio_USD"].sum().sort_values(ascending=False)
+    fig = px.bar(sales_per_category,
+         x = sales_per_category.index,
+         y = sales_per_category,
+         labels = {"y" : "Ventas_USD"},
+         text_auto="0.4s",
+         title = "Ventas en USD por Categoría",
+         color = sales_per_category.index,
+         color_discrete_sequence=['#565DA8', '#56A865', '#A28FB7', '#aec7e8']
+    )
 
-    fig = px.bar(discount_by_status, text_auto=".1f", title="Descuento aplicado por cada suscripción",
-                 color_discrete_sequence=["#6F04E8", "#04E82A"], template="plotly_dark",
-                 labels={"Estado_de_Suscripción": "Estado de Suscripción", "value": "Porcentaje (%)"}
-        )
     fig.update_layout(
-     title={"font": {"size": 20, "family": "tahoma"}, "x": 0.5, "xanchor": "center"},
-        xaxis={"title": 'Estado de suscripción', "tickfont": {"size": 15}, "title_font": {"size": 18}},
-        yaxis={"title": 'Porcentaje (%)', "tickfont": {"size": 15}, "title_font": {"size": 18}},
-        legend_title_text='Descuento Aplicado',    
-        )
+        showlegend=False,
+        xaxis={"title": 'Categoría', "tickfont": {"size": 15, "family": 'Arial'}, "title_font": {"size": 20}},
+        yaxis={"title": 'Ventas (USD)', "tickfont": {"size": 15, "family": 'Arial'}, "title_font": {"size": 20}},
+        title={"font": {"size": 25, "family": "Arial"}, "x": 0.5, "xanchor": 'center'}
+    )
     fig.update_traces(
-        textfont={"family": "tahoma", "size": 16, "color": "white"},
-        hovertemplate="Subscription Status: %{x}<br>Total Purchases: %{value:.4s}",
-        )
-    return fig
-
-def create_shipping_chart(the_df):
-    hist_data = [the_df['Edad'].dropna()]
-    group_labels = ['Edades']
-    fig = ff.create_distplot(hist_data, group_labels, bin_size=5, curve_type='kde',
-                            show_hist=True, show_rug=False, colors=['#26DB2B'])
-    mean_age = the_df['Edad'].mean()
-    fig.add_vline(x=mean_age, line=dict(color='#8000FF', dash='dash'), 
-                 annotation_text=f'Edad Promedio ({mean_age:.0f})', annotation_position='top left')
-    fig.update_layout(
-        title={"text": 'Distribución de Edades con Curva de Densidad', "font": {"size": 25}, "x": 0.5, "xanchor": 'center'},
-        xaxis={"title": 'Edad', "title_font": {"size": 18}},
-        yaxis={"title": 'Densidad', "title_font": {"size": 18}},
-        showlegend=False,   
+        textfont={"family": "tahoma", "size": 16, "color": "#fff"},
+        hovertemplate="Categoría:%{x}<br>Ventas USD: %{y}"
     )
     return fig
